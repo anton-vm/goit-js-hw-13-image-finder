f5735e00f10461b66a3276901f6c5640c94f68f7
anton-vm <anton_vm@ukr.net>
2020-04-06 21:38:52 +0300

    HW-13-04

--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
 import './styles.css';
-import * as basicLightbox from 'basiclightbox';
-import 'basiclightbox/src/styles/main.scss';
+import formPicture from './templates/form.hbs'
 
 
 
 const id = '15591781-785a03c118f12007382b46528';
 let pageNumber = 1;
-
-
-function renderInputArea() {
-  const inputArea = `<form class="search-form" id="search-form">
-    <input
-      type="text"
-      name="query"
-      autocomplete="off"
-      class="input"
-      placeholder="Search images..."
-    />
-  </form>`;
-  const place = document.querySelector('#root');
-  place.insertAdjacentHTML('afterbegin', inputArea);
-}
-
-renderInputArea();
-
-function createUl() {
-  const ul = document.createElement('ul');
-  ul.classList.add('photo-link');
-  const place = document.querySelector('#root');
-  place.insertAdjacentElement('beforeend', ul);
-}
 
 
 function searchData (e) {
@@ -39,90 +14,27 @@ function searchData (e) {
   }
   const input = document.querySelector('.input').value;
   apiService(input, pageNumber, id);
+  document.querySelector('.input').value = ''
 };
+
+
 
 const apiService = (search, pageNumber, key) => {
   const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=${pageNumber}&per_page=12&key=${key}`;
 
   fetch(URL)
     .then(res => res.json())
-    .then(data => {
-
-      data.hits.map(el => renderItem(el));
-    });
+    .then(data => renderItem(data.hits)
+    );
 };
 
 function renderItem(element) {
-  const item = `  <li class="photo-items">
- <div class="photo-card">
-  <img src="${element.webformatURL}" alt="" class="photo-img" />
 
-  <div class="stats">
-    <p class="stats-item">
-      <i class="material-icons">thumb_up</i>
-      ${element.likes}
-    </p>
-    <p class="stats-item">
-      <i class="material-icons">visibility</i>
-      ${element.views}
-    </p>
-    <p class="stats-item">
-      <i class="material-icons">comment</i>
-      ${element.comments}
-    </p>
-    <p class="stats-item">
-      <i class="material-icons">cloud_download</i>
-      ${element.downloads}
-    </p>
-  </div>
-</div>
-</li>
-`;
+const item = formPicture(element)
+
   const place = document.querySelector('.photo-link');
   place.insertAdjacentHTML('beforeend', item);
 }
-
-// function showPicture(event) {
-//   const instance = basicLightbox.create(`
-//     <img src="${event.target.largeImageURL}" width="800" height="600">
-// `)
-// instance.show()
-// }
-
-
-
-// const pictureClick = document.querySelector('body')
-
-// pictureClick.addEventListener('click', showPicture);
-
-// function showPicture(event) {
-//     if (event.target.nodeName === 'IMG') {
-//         const imageBig = event.target.dataset.source;
-//         console.log(event.target.dataset.source);
-//         console.log(event);
-//         basicLightbox.create(`
-//         <img src="${event.target.dataset.source}">
-//     `).show()
-//     }
-// }
-
-function generateButton() {
-  if (!document.querySelector('.more-btn')) {
-    const btn = `<button class="more-btn">Load more</button>`;
-    const place = document.querySelector('#root');
-    place.insertAdjacentHTML('beforeend', btn);
-    listenBtn();
-  }
-}
-
-function listenBtn() {
-  const btnListen = document.querySelector('.more-btn');
-  btnListen.addEventListener('click', addPage);
-}
-
-// generateButton ()
-
-
 
 
 function addPage() {
@@ -147,8 +59,9 @@ function scroll() {
 
 const form = document.querySelector('#search-form');
 form.addEventListener('submit', searchData);
-form.addEventListener('submit', createUl);
-form.addEventListener('submit', generateButton);
+
+const loadMore = document.querySelector('.more-btn');
+loadMore.addEventListener('click', addPage)
 
 
 
